d3eeaee733c5d3f281f4dc6d841b33a089e0290d
Domingos Elias <101057463+DomingosE@users.noreply.github.com>
2022-10-29 18:53:34 -0300

    Observação - Desenvolvido em Python v 3.10.8
    Esse código foi desenvolvido na versão estável do Python 3.10.8 através da IDE PyCharm Community Edition.

--- questao2.py
+++ questao2.py
@@ -14,7 +14,7 @@ while True:
 
 ValorTotal = (LadoA+LadoB+LadoC)
 
-if LadoA > (LadoB+LadoC) or LadoB > (LadoA+LadoC) or LadoC > (LadoA+LadoB):
+if LadoA >= (LadoB+LadoC) or LadoB >= (LadoA+LadoC) or LadoC >= (LadoA+LadoB):
     print(f'\nSe somarmos os valores do lado "A" = {LadoA}, lado "B" = {LadoB}, e lado "C" = {LadoC} temos o valor total de {ValorTotal}. Esse valor total não forma um triângulo com valores positivos.')
 
 else:
